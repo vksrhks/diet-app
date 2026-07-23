52da70bd83791fcf824c849c2258ccd2ef97582e
vksrhks <seung-hoo.jung@hansl.com>
2026-07-23 16:33:22 +0900

    Mobile UI Optimization

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: any = {
   experimental: {
     serverActions: {
       bodySizeLimit: '5mb',
+      allowedOrigins: ['localhost:3000', '192.168.1.151:3000'],
     },
   },
   eslint: {
